82b98e1c88d9f2b2558c92051b9adaef4153f465
Max Makhrov <makhrov.max@gmail.com>
2023-06-27 09:19:45 +0300

    added dist filder to Git

---
+++ dist/code.js
@@ -0,0 +1,29 @@
+// Apps Script (Servrer) Code goes here
+// Note: we save the file as *.js here, Clasp converts it to *.gs
+
+
+function onOpen(e) {
+    SpreadsheetApp.getUi().createAddonMenu()
+        .addItem("Show sidebar", "showSidebar")
+        /* more menu items if needed */
+        .addToUi();
+}
+function onInstall(e) {
+    onOpen(e);
+}
+function showSidebar() {
+    var ui = HtmlService.createTemplateFromFile("dist/ui/index")
+        .evaluate()
+        .setTitle("Add-On Menu Name");
+    SpreadsheetApp.getUi().showSidebar(ui);
+}
+
+
+/**
+ * Say "Hello"
+ * @param {String} message 
+ */
+function beep(message) {
+    Browser.msgBox(message);
+    return 'Apps Script';
+}
